f537f891a93b9abc787050b8e13794ddffb60290
Blake Rayvid <Blake@br-macbook.local>
2016-07-29 12:07:09 -0400

    test

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,26 +1,38 @@
 function setup() {
   createCanvas(1200,700);
 }
-
+// Position (constantly updated)
 var xPos = 100;
 var yPos = 700;
+
+// First position
 xInit = xPos;
 yInit = yPos;
+
+// Constants
 var g = .01;
-var t = 1;
-var xInitVel = 1;
+
+// Velocities
+var xInitVel = 4;
 var yInitVel = 3;
+
+// Time counter
+var t = 1;
+
 
 function draw() {
   
+if(yPos <= 700 && xPos <= 1200){
   background(0);
   fill(50,255,50);
   ellipse(xPos, yPos, 10,10);
   incPos(xPos, yPos);
-  
-  if(xPos>=1200){
-    return;
-  }
+}else{
+    xPos = xInit;
+    yPos = yInit;
+    t=1;
+}
+
 }
 
 function incPos(x,y){
