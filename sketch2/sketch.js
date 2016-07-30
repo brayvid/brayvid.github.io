a7c5b66412349aaf9ba08a74502863cb4af0989f
Blake Rayvid <Blake@br-macbook.local>
2016-07-30 13:36:27 -0400

    upd

--- sketch2/sketch.js
+++ sketch2/sketch.js
@@ -7,5 +7,5 @@ function draw() {
   background(0);
   
   fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
-  ellipse(window.innerWidth/2,window.innerHeight/2,500,500);
+  ellipse(window.innerWidth/2,window.innerHeight/2,500, 500);
 }
