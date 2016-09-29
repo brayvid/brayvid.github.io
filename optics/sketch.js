ae7409a57ec6eeb8b93909c1409e332ca8eade1c
brayvid <Blake@br-macbook.local>
2016-09-29 12:35:59 -0400

    fix

--- optics/sketch.js
+++ optics/sketch.js
@@ -1,4 +1,4 @@
-var object_height = 10;
+var object_height = 25;
 var object_distance;
 
 var image_height;
@@ -13,18 +13,36 @@ var focal_length;
 
 
 function setup() {
+
   frameRate(0.5);
   createCanvas(600, 400);
-  object_distance = 50;
-  radius = 35;
+  object_distance = 100;
+  radius = 70;
 
-  image_height = 1;
-  image_distance = 1;
+  image_height = 0;
+  image_distance = 0;
   focal_length = 1;
+
+  origin = createVector(width/2,height/2);
+
+  objDist = p5.Vector.add(origin,-1*object_distance);
+
+  objArrow = new Arrow(origin,objDist);
+
+  objArrow.color = color(0);
+  objArrow.draggable = false;
+  objArrow.grab = false;
+  objArrow.width = 4;
+
+  objBase = createVector();
+
 
 }
 
 function draw() {
+  background(180);
+  objArrow.update();
+  objArrow.display();
 
 
 
@@ -59,6 +77,7 @@ function pr() {
 
 function keyPressed() {
   if (keyCode == LEFT_ARROW) {
+    // objArrow.add(1);
     object_distance += 1;
   } else if (keyCode == RIGHT_ARROW) {
     object_distance -= 1;
@@ -72,7 +91,7 @@ function keyPressed() {
 
   updateFocus(radius);
   updateHeight(object_height, object_distance, image_distance);
-  updateDistance(focal_length, image_distance);
+  updateDistance(focal_length, object_distance);
   pr();
   return false; // prevent default
 }
