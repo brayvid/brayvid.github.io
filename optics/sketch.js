4665e6bbeafa632d89050bd13bca323bd800a0dd
brayvid <Blake@br-macbook.local>
2016-10-14 12:09:07 -0400

    upd

--- optics/sketch.js
+++ optics/sketch.js
@@ -76,6 +76,8 @@ function draw() {
     textSize(20);
     textAlign(CENTER);
     text('Focal length',3*width/4-3,height-110);
+    noFill();
+    rect(3*width/4-100,height-100,3*width/4+90,height-78);
     pop();
     // push();
     // stroke('rgba(0,0,0,0.1)');
