e27dd713a8ceb49322122fb5d86ea459bd21a6ea
brayvid <Blake@br-macbook.local>
2016-10-15 19:11:20 -0400

    add orientation

--- swipe/sketch.js
+++ swipe/sketch.js
@@ -4,6 +4,7 @@ var endDist;
 var newVelocity;
 var beginTime;
 var endTime;
+var globalAcc;
 
 function FreeBodyMover(p, v, a, m, c){
 	// Properties
@@ -27,42 +28,40 @@ function FreeBodyMover(p, v, a, m, c){
 	this.act = function(force){
 		// add a force to the array of acting forces
 		this.fbd.forces[this.fbd.forces.length+1] = force;
-
-	};
-
+	}; //end act()
 
 	this.refresh = function(){
+		  	// "bounceEdges" from science.js by http://github.com/hedbergj
+			  if(this.position.x < 0+this.mass/2){
+				 overinx = this.position.x-this.mass/2;
+				 vatwidth = Math.sqrt(Math.pow(this.velocity.x,2)-2*this.acceleration.x*overinx);
+				 this.velocity.x = 1*vatwidth;
+				 this.position.x = 0+this.mass/2; 
+			 	 }
+			  if(this.position.x > width-this.mass/2){
+			    overinx = this.position.x-width+this.mass/2;
+			    vatwidth = Math.sqrt(Math.pow(this.velocity.x,2)-2*this.acceleration.x*overinx);
+			    this.position.x = width-this.mass/2;
+			    this.velocity.x = -1*vatwidth;
+			  }
 
-		// Check edges by http://github.com/hedbergj
-		  if(this.position.x < 0+this.mass/2){
-			 overinx = this.position.x-this.mass/2;
-			  vatwidth = Math.sqrt(Math.pow(this.velocity.x,2)-2*this.acceleration.x*overinx);
-			  this.velocity.x = 1*vatwidth;
-			  this.position.x = 0+this.mass/2; 
-		 	 }
-		  if(this.position.x > width-this.mass/2){
-		    overinx = this.position.x-width+this.mass/2;
-		    vatwidth = Math.sqrt(Math.pow(this.velocity.x,2)-2*this.acceleration.x*overinx);
-		    this.position.x = width-this.mass/2;
-		    this.velocity.x = -1*vatwidth;
-		  }
+			  if(this.position.y < 0+this.mass/2){
+			    overiny = this.position.y-this.mass/2;
+			    vatheight = Math.sqrt(Math.pow(this.velocity.y,2)-2*this.acceleration.y*overiny);
+			    this.velocity.y = 1*vatheight;
+			    this.position.y = 0+this.mass/2;
+			  }
 
-		  if(this.position.y < 0+this.mass/2){
-		    overiny = this.position.y-this.mass/2;
-		    vatheight = Math.sqrt(Math.pow(this.velocity.y,2)-2*this.acceleration.y*overiny);
-		    this.velocity.y = 1*vatheight;
-		    this.position.y = 0+this.mass/2;
+			  if(this.position.y > height-this.mass/2){
+			    overiny = this.position.y-height+this.mass/2;
+			    vatheight = Math.sqrt(Math.pow(this.velocity.y,2)-2*this.acceleration.y*overiny);
+			    this.position.y = height-this.mass/2;
+			    this.velocity.y = -1*vatheight;
+			  }	
+			 // End bounceEdges
 
-		  }
-		  if(this.position.y > height-this.mass/2){
-		    overiny = this.position.y-height+this.mass/2;
-		    vatheight = Math.sqrt(Math.pow(this.velocity.y,2)-2*this.acceleration.y*overiny);
-		    this.position.y = height-this.mass/2;
-		    this.velocity.y = -1*vatheight;
-		  }
-		  // End check edges
-		
-	   		// Recalculate position and velocity
+	   		// Recalculate position then velocity 
+	   		this.acceleration = p5.Vector.add(createVector(0,0),globalAcc);
 			this.velocity = p5.Vector.add(this.velocity,this.acceleration);
 			this.position = p5.Vector.add(this.position,this.velocity);
 
@@ -71,47 +70,54 @@ function FreeBodyMover(p, v, a, m, c){
 			fill(this.color);
 			ellipse(this.position.x,this.position.y,this.mass,this.mass);
 			pop();
-	};
+	};//end refresh()
 }
 
-
 function setup(){
+
+	
 	frameRate(60);
 	createCanvas(windowWidth,windowHeight);
-
 	balls = [];
+	globalAcc = createVector(0,0);
 }
 
 function draw(){
 	background(255);
+
+	// Recalculate acceleration
+	globalAcc.x = map(constrain(rotationY,-45,45),-20,20,-0.05,0.05);
+	globalAcc.y = map(constrain(rotationX,-45,45),-20,20,-0.05,0.05);
+
+	// Update balls
 	for(var i = 0; i < balls.length; i++){
 		balls[i].refresh();
 	}
-}
 
+	// Limit number of balls created
+	if(balls.length > 9){
+		for(var i = 0; i < balls.length-1; i++){
+		balls.splice(i,1);
+		}
+	}
+	console.log(rotationX +', '+rotationY+', '+rotationZ);
+}
 
 function touchStarted(){
 	beginDist = createVector(mouseX,mouseY);
 	beginTime = millis();
-
 }
 
 function touchEnded(){
 	endDist = createVector(mouseX, mouseY);
 	endTime = millis();
-
 	var interval = endTime - beginTime;
-
 	newVelocity = p5.Vector.div(p5.Vector.mult(p5.Vector.sub(endDist,beginDist),4),(endTime-beginTime)/8);
-
-	balls[balls.length] = new FreeBodyMover(createVector(mouseX,mouseY),createVector(newVelocity.x,newVelocity.y),createVector(0,2),50,color(random(0,255),random(0,255),random(0,255)));
+	balls[balls.length] = new FreeBodyMover(createVector(mouseX,mouseY),createVector(newVelocity.x,newVelocity.y),createVector(globalAcc.x,globalAcc.y),50,color(random(0,255),random(0,255),random(0,255)));
 }
-
 
 function windowResized(){
 	resizeCanvas(windowWidth,windowHeight);
+	// Something else
 	
 }
-
-
-
