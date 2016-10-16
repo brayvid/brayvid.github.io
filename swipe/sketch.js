e81b19891809c3ce2d132d48edcc38de7b5f9161
brayvid <Blake@br-macbook.local>
2016-10-16 16:29:45 -0400

    add collisions

--- swipe/sketch.js
+++ swipe/sketch.js
@@ -18,12 +18,18 @@ function FreeBodyMover(p, v, a, m, c){
 	this.velocity = v;
 	this.acceleration = a;
 	this.mass = m;
+	this.size = 75;
 	this.color = c;
 	this.appliedForce = p5.Vector.mult(this.acceleration,this.mass);
 	this.forceArrow = new Arrow(this.position,p5.Vector.add(this.position,this.appliedForce));
 	this.forceArrow.color = this.color;
 	this.forceArrow.grab = false;
 	this.forceArrow.draggable = false;
+
+	this.velocityArrow = new Arrow(this.position,p5.Vector.add(this.position,this.velocity));
+	this.velocityArrow.color = this.color;
+	this.velocityArrow.grab = false;
+	this.velocityArrow.draggable = false;
 
 	this.momentum = p5.Vector.add(this.velocity,this.mass);
 	this.kineticEnergy = 0.5 * this.mass * (Math.pow(this.velocity.x,2)+Math.pow(this.velocity.y,2));
@@ -65,7 +71,6 @@ function FreeBodyMover(p, v, a, m, c){
 			  }	
 			 // End bounceEdges
 
-
 	   		// Recalculate position then velocity 
 	   		this.acceleration = p5.Vector.add(createVector(0,0),globalAcc);
 			this.velocity = p5.Vector.add(this.velocity,this.acceleration);
@@ -79,14 +84,28 @@ function FreeBodyMover(p, v, a, m, c){
 			this.forceArrow.target = p5.Vector.add(this.position,this.appliedForce);
 			this.forceArrow.display();
 
+			// Draw velocity arrow
+			this.velocityArrow.origin = this.position;
+			this.velocityArrow.target = p5.Vector.add(this.position,this.velocity);
+			this.velocityArrow.display();
+
 
 			// Redraw
 			push();
 			fill(this.color);
-			ellipse(this.position.x,this.position.y,this.mass,this.mass);
+			ellipse(this.position.x,this.position.y,this.mass/2,this.mass/2);
 			pop();
 
 	}; // end update method
+
+	this.intersects = function(other){
+		var d = dist(this.position.x,this.position.y,other.position.x,other.position.y);
+		if(d < (this.mass/4) + (other.mass/4)){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }// End FreeBodyMover object
 
 
@@ -149,6 +168,64 @@ function draw(){
 	globalAcc.x = map(constrain(rotationY,-45,45),-45,45,-2,2);
 	globalAcc.y = map(constrain(rotationX,-45,45),-45,45,-2,2);
 
+	// Collisions
+	for(var i = 0; i < balls.length; i++){
+		for(var j = 0; j < balls.length; j++){
+			if(i != j && balls[i].intersects(balls[j])){
+
+				var collisionPoint = createVector(
+					// X coord
+					((balls[i].position.x * balls[j].mass/2) + (balls[j].position.x * balls[i].mass/2)) 
+				 	/ (balls[i].mass/2 + balls[j].mass/2),
+				 	// Y coord
+				 	((balls[i].position.y * balls[j].mass/2) + (balls[j].position.y * balls[i].mass/2)) 
+				 	/ (balls[i].mass/2 + balls[j].mass/2)
+				);
+
+				// var heading1 = degrees(balls[i].velocity.heading());
+				// var heading2 = degrees(balls[j].velocity.heading());
+				// console.log(balls[i] + ' intersects ' + balls[j]);
+				// console.log('heading 1: '+ heading1 + ', heading 2: '+ heading2);
+				// console.log(collisionPoint.x + ", "+ collisionPoint.y);
+				// ellipse(collisionPoint.x,collisionPoint.y,10,10);
+
+				// Calculate resultant velocities
+				var newVelX1 = (balls[i].velocity.x * (balls[i].mass - balls[j].mass) + (2 * balls[j].mass * balls[j].velocity.x)) / (balls[i].mass + balls[j].mass);
+				var newVelY1 = (balls[i].velocity.y * (balls[i].mass - balls[j].mass) + (2 * balls[j].mass * balls[j].velocity.y)) / (balls[i].mass + balls[j].mass);
+				var newVelX2 = (balls[j].velocity.x * (balls[j].mass - balls[i].mass) + (2 * balls[i].mass * balls[i].velocity.x)) / (balls[i].mass + balls[j].mass);
+				var newVelY2 = (balls[j].velocity.y * (balls[j].mass - balls[i].mass) + (2 * balls[i].mass * balls[i].velocity.y)) / (balls[i].mass + balls[j].mass);
+
+				// Update positions to prevent sticking
+				balls[i].position.x = balls[i].position.x + newVelX1;
+				balls[j].position.x = balls[j].position.x + newVelX2;
+				balls[i].position.y = balls[i].position.y + newVelY1;
+				balls[j].position.y = balls[j].position.y + newVelY2;
+
+				// Update velocities with calculated ones
+				balls[i].velocity = createVector(newVelX1,newVelY1);
+				balls[j].velocity = createVector(newVelX2,newVelY2);
+				
+
+
+				// var collisionMomentum = p5.Vector.add(balls[i].momentum,balls[j].momentum);
+				// // var angle = p5.Vector.angleBetween(balls[i].velocity,balls[j].velocity);
+
+
+				
+				// // Finish collision math
+
+				
+				// // console.log(180*angle/PI);
+				
+
+
+
+
+
+
+			}
+		}
+	}
 
 	// if(false){
 	// 	if(deviceOrientation == 'portrait' && rotationX > 0){ // normal phone orientation
@@ -222,10 +299,11 @@ function touchEnded(){
 	newVelocity = p5.Vector.div(p5.Vector.mult(p5.Vector.sub(endDist,beginDist),4),interval/4);
 
 	// Make a new ball
-	balls[balls.length] = new FreeBodyMover(createVector(mouseX,mouseY),createVector(newVelocity.x,newVelocity.y),createVector(globalAcc.x,globalAcc.y),map(interval,0,1000,25,150),randColor);
 	randColor = color(random(0,255),random(0,255),random(0,255),random(100,200));
+	balls[balls.length] = new FreeBodyMover(createVector(mouseX,mouseY),createVector(newVelocity.x,newVelocity.y),createVector(globalAcc.x,globalAcc.y),map(interval,0,500,25,150),randColor);
 }
 
 function windowResized(){
 	resizeCanvas(windowWidth,windowHeight);
 }
+
