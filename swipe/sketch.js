67af368cb2e488fcca7f84eff20f94ad86c50cd7
brayvid <Blake@br-macbook.local>
2016-10-17 01:48:07 -0400

    pause

--- swipe/sketch.js
+++ swipe/sketch.js
@@ -27,6 +27,12 @@ var touchTime;
 
 var randColor;
 
+var pausedSpheres;
+
+var wasPaused = false;
+
+var tempIsSet = false;
+
 function Sphere(p, v, a, m, c){
 
 	// Properties
@@ -110,6 +116,7 @@ function Sphere(p, v, a, m, c){
 			this.velocityArrow.target.y = this.position.y + map(this.velocity.y,-200,200,-150,150);
 			this.velocityArrow.display();
 
+
 			// Redraw
 			push();
 			fill(this.color);
@@ -161,7 +168,7 @@ function setup(){
 	fps = frameRate();
 	createCanvas(windowWidth,windowHeight);
 	spheres = [];
-	globalAccel = createVector(0,20);
+	globalAccel = createVector(0,0);
 	globalAccelOn = true;
 	collisionsOn = true;
 	dissipation = 0.88;
@@ -171,13 +178,15 @@ function setup(){
 	collisionCount = 0;
 	stopAll = false;
 	maxSpheres = 50;
+	pausedSpheres = [];
+
 
 	for(var i = 0; i < 5; i++){
 		spheres[i] = new Sphere(
 					createVector(random(0,width),random(0,height)),
 					createVector(random(0,25),random(0,25)),
 					createVector(random(-1,1),random(-1,1)),
-					random(0,75),
+					random(50,90),
 					color(random(0,255),random(0,255),random(0,255),random(100,200)));
 	}
 }
@@ -186,228 +195,245 @@ function setup(){
 function draw(){
 	background(255);
 
-	// console.log('deviceHasmoved == '+deviceHasMoved);
-	// push();
-	// fill(randColor);
-	// ellipse(mouseX,mouseY,50,50);
-	// pop();
-
-	// Get time for intervals
-	currentTime = millis();
-
-	// Display framerate
-	push();
-	textSize(24);
-	if(round(currentTime) % 10 == 0){
-		fps = round(frameRate());
-	}
-	text(fps + ' fps',60, height-25);
-	pop();
-	
-	// Display total kinetic energy
-	push();
-	textSize(24);
-	textAlign(CENTER);
-	var totalKE = 0;
-	for(var i = 0; i < spheres.length; i++){
-		totalKE += spheres[i].kineticEnergy;
-	}
-	text('KE:',width-75,height-45);
-	text(round(totalKE) + ' J',width-75,height-20);
-	pop();
-
-	// What to do before any user interaction
-	if(!started){
-		push();
-		textAlign(CENTER);
-		textSize(width/15);
-		fill(25);
-
-		// Check if on mobile or desktop
-		if(deviceHasMoved){
+		// Intro screen
+		if(!started && !stopAll){
 			push();
-			textSize(width/20);
-			text('Lock your screen rotation',width/2,(height/2)-40);
-			pop();
+			textAlign(CENTER);
+			textSize(width/15);
+			fill(25);
 
-			// Gravity on/off switch
-			// pop();
-			// push();
-			// rectMode(CORNER);
-			// fill(0,230,0);
-			// rect(25,height-550,width/2-30,150);
-			// fill(230,0,0);
-			// rect(width/2+30,height-550,width/2-60,150);
-			// pop();
+			// Check if on mobile or desktop
+			if(deviceHasMoved){
+				push();
+				textSize(width/20);
+				text('Lock your screen rotation',width/2,(height/2)-40);
+				pop();
 
-			// push();
-			// fill(255);
-			// textSize(60);
-			// noStroke();
-			// textAlign(CENTER);
-			// text('GRAVITY ON',(35+(width/2-30))/2, height-450);
-			// text('GRAVITY OFF',((width/2+30)+(width/2-60)/2), height-450);
+				// Gravity on/off switch
+				// pop();
+				// push();
+				// rectMode(CORNER);
+				// fill(0,230,0);
+				// rect(25,height-550,width/2-30,150);
+				// fill(230,0,0);
+				// rect(width/2+30,height-550,width/2-60,150);
+				// pop();
 
-		}else{
-			push();
-			textSize(width/25);
-			fill(225,0,75,125);
-			text('Gravity off - needs accelerometers.',width/2,(height/2)-70);
-			pop();
-		}
-		// Displayed on both types of devices
-		text('Tap or drag to begin',width/2,(height/2)+30);
-		
-	}else{
-		push();
-		textSize(24);
-		textAlign(CENTER);
+				// push();
+				// fill(255);
+				// textSize(60);
+				// noStroke();
+				// textAlign(CENTER);
+				// text('GRAVITY ON',(35+(width/2-30))/2, height-450);
+				// text('GRAVITY OFF',((width/2+30)+(width/2-60)/2), height-450);
 
-		text(spheres.length + "/" + maxSpheres,width-50,50);
-		pop();
-	}
-
-	// Only calculate if enabled
-	if(globalAccelOn){
-		//Get acceleration from device rotation data
-		globalAccel.x = map(constrain(rotationY,-45,45),-45,45,-2,2);
-		globalAccel.y = map(constrain(rotationX,-45,45),-45,45,-2,2);
-	}
-
-	// Only check if enabled
-	if(collisionsOn){
-
-		for(var i = 0; i < spheres.length; i++){
-			for(var j = 0; j < spheres.length; j++){
-
-				// Spheres perform the check themselves
-				if(i != j && spheres[i].intersects(spheres[j])){
-
-					// Old
-					// var heading1 = degrees(spheres[i].velocity.heading());
-					// var heading2 = degrees(spheres[j].velocity.heading());
-					// console.log(spheres[i] + ' intersects ' + spheres[j]);
-					// console.log('heading 1: '+ heading1 + ', heading 2: '+ heading2);
-					// console.log(collisionPoint.x + ", "+ collisionPoint.y);
-
-					// // Draw collision points
-					// push();
-					// fill(255,0,0);
-					// var r = map(collisionMomentum,0,1,0,50);
-					// ellipse(collisionPoint.x,collisionPoint.y,r,r);
-					// pop();
-
-					// COLLISION PHYSICS
-					// Calculate resultant velocities
-					var newVelX1 = (spheres[i].velocity.x * (spheres[i].mass - spheres[j].mass) + (2 * spheres[j].mass * spheres[j].velocity.x)) / (spheres[i].mass + spheres[j].mass);
-					var newVelY1 = (spheres[i].velocity.y * (spheres[i].mass - spheres[j].mass) + (2 * spheres[j].mass * spheres[j].velocity.y)) / (spheres[i].mass + spheres[j].mass);
-					var newVelX2 = (spheres[j].velocity.x * (spheres[j].mass - spheres[i].mass) + (2 * spheres[i].mass * spheres[i].velocity.x)) / (spheres[i].mass + spheres[j].mass);
-					var newVelY2 = (spheres[j].velocity.y * (spheres[j].mass - spheres[i].mass) + (2 * spheres[i].mass * spheres[i].velocity.y)) / (spheres[i].mass + spheres[j].mass);
-
-					// Update positions to prevent sticking
-					spheres[i].position.x = spheres[i].position.x + newVelX1;
-					spheres[j].position.x = spheres[j].position.x + newVelX2;
-					spheres[i].position.y = spheres[i].position.y + newVelY1;
-					spheres[j].position.y = spheres[j].position.y + newVelY2;
-
-					// Update velocities with calculated ones
-					spheres[i].velocity.set(newVelX1,newVelY1);
-					spheres[j].velocity.set(newVelX2,newVelY2);
-
-
-
-					var collisionPoint = createVector(
-						// X
-						((spheres[i].position.x * spheres[j].mass/2) + (spheres[j].position.x * spheres[i].mass/2)) 
-					 	/ (spheres[i].mass/2 + spheres[j].mass/2),
-					 	// Y
-					 	((spheres[i].position.y * spheres[j].mass/2) + (spheres[j].position.y * spheres[i].mass/2)) 
-					 	/ (spheres[i].mass/2 + spheres[j].mass/2)
-					);
-
-					var collisionMomentum = p5.Vector.add(spheres[i].momentum,spheres[j].momentum);
-
-
-					spheres[i].newColor(spheres[j]);
-
-					// Increment count
-					collisionCount++;
-
-
-					// console.log('collisions: ' + collisionCount);
-
-					// // var angle = p5.Vector.angleBetween(spheres[i].velocity,spheres[j].velocity);
-
-					// // Finish collision math
-					
-					// // console.log(180*angle/PI);
-				}
+			}else{
+				push();
+				textSize(width/25);
+				fill(225,0,75,125);
+				text('Gravity off - needs accelerometer.',width/2,(height/2)-70);
+				pop();
 			}
+			// Displayed on both types of devices
+			text('Tap or drag anywhere',width/2,(height/2)+30);
+			
 		}
-	} // end collisions section
-
-	// Account for device rotation -- incomplete
-	// if(false){
-	// 	if(deviceOrientation == 'portrait' && rotationX > 0){ // normal phone orientation
-	// 		globalAccel.x = map(constrain(rotationY,-50,50),-50,50,-0.8,0.8);
-	// 		globalAccel.y = map(constrain(rotationX,-50,50),-50,50,-0.8,0.8);
-
-	// 	}else if(rotationX < 0){ // Upside down portrait
-	// 		globalAccel.x = map(constrain(rotationY,-50,50),-50, 50,0.8,-0.8);
-	// 		globalAccel.y = map(constrain(rotationX,-50,50),-50, 50,0.8,-0.8); // flip signs
-
-	// 	}else if(deviceOrientation == 'landscape' && rotationY > 0){  // Clockwise landscape
-	// 		globalAccel.x = map(constrain(rotationX,-50,50),-50,50,0.8,-0.8); 
-	// 		globalAccel.y = map(constrain(rotationY,-50,50),-50,50,-0.8,0.8);
-
-	// 	}else if(rotationY < 0){ // Counterclockwise landscape
-	// 		globalAccel.x = map(constrain(rotationX,-50,50),-50,50,0.8,-0.8);
-	// 		globalAccel.y = map(constrain(rotationY,-50,50),50,-50,-0.8,0.8);
-	// 	}else{
-
-	// 	}
-	// }
-
-	// if(deviceOrientation == 'landscape' && rotationY < 0){
-	// globalAccel.x = map(constrain(rotationX,-50,50),-50,50,-0.2,0.2);
-	// globalAccel.y = map(constrain(rotationY,-50,50),-50,50,-0.2,0.2);
-	// }
-
-	// if(deviceOrientation == 'landscape' && rotationY > 0){
-	// globalAccel.x = map(constrain(rotationY,-50,50),-50,50,0.2,-0.2);
-	// globalAccel.y = map(constrain(rotationX,-50,50),-50,50,-0.2,0.2);
-	// }
-
-
-	// Only update if not paused
-	if(!stopAll){
-		for(var i = 0; i < spheres.length; i++){
-			spheres[i].refresh();
-		}
-	}
 
 	// Pause screen
 	if(stopAll){
+
+		if(!tempIsSet){
+			// Store current spheres in temp array
+			for(var i = 0; i < spheres.length; i++){
+				pausedSpheres[i] = spheres[i];
+			}
+			tempIsSet = true;
+		}
 		push();
 		textAlign(CENTER);
 		textSize(80);
 		fill(0,0,0,90);
 		text('paused',width/2,height/2);
 		pop();
+
+	}else{
+	// Normal operations
+
+			tempIsSet = false;
+
+			//Get acceleration from device rotation data
+			if(globalAccelOn && !stopAll){
+				globalAccel.x = map(constrain(rotationY,-45,45),-45,45,-2,2);
+				globalAccel.y = map(constrain(rotationX,-45,45),-45,45,-2,2);
+			}
+
+			for(var i = 0; i < spheres.length; i++){
+				spheres[i].refresh();
+			}
+
+			// Limit # of spheres
+			if(spheres.length > maxSpheres){
+				spheres.splice(0,26);
+			}
+
+				// Only check if enabled
+			if(collisionsOn){
+
+					for(var i = 0; i < spheres.length; i++){
+						for(var j = 0; j < spheres.length; j++){
+
+							// Spheres perform the check themselves
+							if(i != j && spheres[i].intersects(spheres[j])){
+
+								// Old
+								// var heading1 = degrees(spheres[i].velocity.heading());
+								// var heading2 = degrees(spheres[j].velocity.heading());
+								// console.log(spheres[i] + ' intersects ' + spheres[j]);
+								// console.log('heading 1: '+ heading1 + ', heading 2: '+ heading2);
+								// console.log(collisionPoint.x + ", "+ collisionPoint.y);
+
+								// // Draw collision points
+								// push();
+								// fill(255,0,0);
+								// var r = map(collisionMomentum,0,1,0,50);
+								// ellipse(collisionPoint.x,collisionPoint.y,r,r);
+								// pop();
+
+								// COLLISION PHYSICS
+								// Calculate resultant velocities
+								var newVelX1 = (spheres[i].velocity.x * (spheres[i].mass - spheres[j].mass) + (2 * spheres[j].mass * spheres[j].velocity.x)) / (spheres[i].mass + spheres[j].mass);
+								var newVelY1 = (spheres[i].velocity.y * (spheres[i].mass - spheres[j].mass) + (2 * spheres[j].mass * spheres[j].velocity.y)) / (spheres[i].mass + spheres[j].mass);
+								var newVelX2 = (spheres[j].velocity.x * (spheres[j].mass - spheres[i].mass) + (2 * spheres[i].mass * spheres[i].velocity.x)) / (spheres[i].mass + spheres[j].mass);
+								var newVelY2 = (spheres[j].velocity.y * (spheres[j].mass - spheres[i].mass) + (2 * spheres[i].mass * spheres[i].velocity.y)) / (spheres[i].mass + spheres[j].mass);
+
+								// Update positions to prevent sticking
+								spheres[i].position.x = spheres[i].position.x + newVelX1;
+								spheres[j].position.x = spheres[j].position.x + newVelX2;
+								spheres[i].position.y = spheres[i].position.y + newVelY1;
+								spheres[j].position.y = spheres[j].position.y + newVelY2;
+
+								// Update velocities with calculated ones
+								spheres[i].velocity.set(newVelX1,newVelY1);
+								spheres[j].velocity.set(newVelX2,newVelY2);
+
+
+
+								var collisionPoint = createVector(
+									// X
+									((spheres[i].position.x * spheres[j].mass/2) + (spheres[j].position.x * spheres[i].mass/2)) 
+								 	/ (spheres[i].mass/2 + spheres[j].mass/2),
+								 	// Y
+								 	((spheres[i].position.y * spheres[j].mass/2) + (spheres[j].position.y * spheres[i].mass/2)) 
+								 	/ (spheres[i].mass/2 + spheres[j].mass/2)
+								);
+
+								var collisionMomentum = p5.Vector.add(spheres[i].momentum,spheres[j].momentum);
+
+
+								spheres[i].newColor(spheres[j]);
+
+								// Increment count
+								collisionCount++;
+
+
+								// console.log('collisions: ' + collisionCount);
+
+								// // var angle = p5.Vector.angleBetween(spheres[i].velocity,spheres[j].velocity);
+
+								// // Finish collision math
+								
+								// // console.log(180*angle/PI);
+							}
+						}
+					}
+			} // end collisions section
+
+
+			// Account for device rotation -- incomplete
+			// if(false){
+			// 	if(deviceOrientation == 'portrait' && rotationX > 0){ // normal phone orientation
+			// 		globalAccel.x = map(constrain(rotationY,-50,50),-50,50,-0.8,0.8);
+			// 		globalAccel.y = map(constrain(rotationX,-50,50),-50,50,-0.8,0.8);
+
+			// 	}else if(rotationX < 0){ // Upside down portrait
+			// 		globalAccel.x = map(constrain(rotationY,-50,50),-50, 50,0.8,-0.8);
+			// 		globalAccel.y = map(constrain(rotationX,-50,50),-50, 50,0.8,-0.8); // flip signs
+
+			// 	}else if(deviceOrientation == 'landscape' && rotationY > 0){  // Clockwise landscape
+			// 		globalAccel.x = map(constrain(rotationX,-50,50),-50,50,0.8,-0.8); 
+			// 		globalAccel.y = map(constrain(rotationY,-50,50),-50,50,-0.8,0.8);
+
+			// 	}else if(rotationY < 0){ // Counterclockwise landscape
+			// 		globalAccel.x = map(constrain(rotationX,-50,50),-50,50,0.8,-0.8);
+			// 		globalAccel.y = map(constrain(rotationY,-50,50),50,-50,-0.8,0.8);
+			// 	}else{
+
+			// 	}
+			// }
+
+			// if(deviceOrientation == 'landscape' && rotationY < 0){
+			// globalAccel.x = map(constrain(rotationX,-50,50),-50,50,-0.2,0.2);
+			// globalAccel.y = map(constrain(rotationY,-50,50),-50,50,-0.2,0.2);
+			// }
+
+			// if(deviceOrientation == 'landscape' && rotationY > 0){
+			// globalAccel.x = map(constrain(rotationY,-50,50),-50,50,0.2,-0.2);
+			// globalAccel.y = map(constrain(rotationX,-50,50),-50,50,-0.2,0.2);
+			// }
+
+
+			// console.log('x: ' + rotationX +',y: '+rotationY);
+
+			// if(spheres.length > 0){
+			// 	console.log(spheres[0].acceleration);
+			// }
+
+			// Get time for intervals
+			currentTime = millis();
+
+				// Display framerate
+			push();
+			textSize(24);
+			if(round(currentTime) % 10 == 0){
+				fps = round(frameRate());
+			}
+			text(fps + ' fps',60, height-25);
+			pop();
+			
+			// Display total kinetic energy
+			push();
+			textSize(24);
+			textAlign(CENTER);
+			var totalKE = 0;
+			for(var i = 0; i < spheres.length; i++){
+				totalKE += spheres[i].kineticEnergy;
+			}
+			text('KE:',width-75,height-45);
+			text(round(totalKE) + ' J',width-75,height-20);
+			pop();
+
+			// Display number used
+			push();
+			textSize(24);
+			textAlign(CENTER);
+			text(spheres.length + "/" + maxSpheres,width-50,50);
+			pop();
+
+		}
+
+	if(pausedSpheres.length != 0 && !stopAll){
+		for(var i = 0; i < pausedSpheres.length; i++){
+			spheres[i] = pausedSpheres[i];
+		}
 	}
 
-	// Limit # of spheres
-	if(spheres.length > maxSpheres){
-		spheres.splice(0,26);
-	}
-
-
-	// console.log('x: ' + rotationX +',y: '+rotationY);
-
-	// if(spheres.length > 0){
-	// 	console.log(spheres[0].acceleration);
-	// }
+		// console.log('deviceHasmoved == '+deviceHasMoved);
+		// push();
+		// fill(randColor);
+		// ellipse(mouseX,mouseY,50,50);
+		// pop();	
 }
+
 
 function touchStarted(){
 
@@ -422,7 +448,7 @@ function touchStarted(){
 	// }
 
 	// Record time of first touch
-	if(!started){
+	if(!started && !stopAll){
 		touchTime = millis();
 		started = true;
 	}
@@ -430,20 +456,24 @@ function touchStarted(){
 	timeStarted = millis();
 	beginDist = createVector(mouseX,mouseY);
 	beginTime = millis();
+
 }
 
 function touchEnded(){
 
+	if(!stopAll){
 	// Calculate initial velocity
 	endDist = createVector(mouseX, mouseY);
 	endTime = millis();
 	var interval = endTime - beginTime;
 	newVelocity = p5.Vector.div(p5.Vector.mult(p5.Vector.sub(endDist,beginDist),4),interval/4);
 
-	// MAKE A NEW SPHERE
-	randColor = color(floor(random(0,255)),floor(random(0,256)),floor(random(0,256)),floor(random(100,200)));
-	spheres[spheres.length] = new Sphere(createVector(mouseX,mouseY),createVector(newVelocity.x,newVelocity.y),createVector(globalAccel.x,globalAccel.y),map(interval,0,200,25,125),randColor);
-	console.log("Speed = " + p5.Vector.mag(newVelocity).toFixed(2)+" pixels per frame");
+	
+		// MAKE A NEW SPHERE
+		randColor = color(floor(random(0,255)),floor(random(0,256)),floor(random(0,256)),floor(random(100,200)));
+		spheres[spheres.length] = new Sphere(createVector(mouseX,mouseY),createVector(newVelocity.x,newVelocity.y),createVector(globalAccel.x,globalAccel.y),map(interval,0,200,25,125),randColor);
+		console.log("Speed = " + p5.Vector.mag(newVelocity).toFixed(2)+" pixels per frame");
+	}
 }
 
 
@@ -456,13 +486,16 @@ function deviceMoved(){
 	deviceHasMoved = true;
 }
 
-// Pause functionality
-function keyTyped(){
+// Pause input
+function keyPressed(){
+
+	// Simply flip stopAll on and off
 	if(keyCode == 32 && !stopAll){
 		stopAll = true;
 	}else if(keyCode == 32 && stopAll){
 		stopAll = false;
 	}
+
 }
 
 
