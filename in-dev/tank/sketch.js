d5bb8c34c71bffaf4014ec4cbfe7990bbc67e683
brayvid <blakerayvid@gmail.com>
2017-06-11 11:17:53 -0400

    upd

--- in-dev/tank/sketch.js
+++ in-dev/tank/sketch.js
@@ -6,6 +6,7 @@ var poundsSaltInTank,
     tankVolume,
     overallSaltRate,
     timeCounter,
+    titleScreen,
     beginSwitch,
     maxTime,
     maxSaltAmount;
@@ -21,21 +22,28 @@ function setup(){
   textSize(24);
   fill(0);
 
-  concentrationInSlider = createSlider(0,0.5,0,0.01);
+  concentrationInSlider = createSlider(0,0.25,0.125,0.025);
   concentrationInSlider.position(width/2-150,height-50);
   concentrationInSlider.size(300);
+  concentrationInSlider.input(concentrationChanged);
+
+  initialSaltSlider = createSlider(0,25,25,1);
+  initialSaltSlider.position(width/4-250,height-50);
+  initialSaltSlider.size(300);
+  initialSaltSlider.input(saltChanged);
 
   /* Edit to change scale */ 
-  maxTime = 2000;
+  maxTime = 800;
   maxSaltAmount = 30;
 
   
-  poundsSaltInTank = 25;
+  poundsSaltInTank = initialSaltSlider.value();
   waterFlowRate = 0.2;
   tankVolume = 100;
 
   concentrationIn = concentrationInSlider.value();
   timeCounter = 1;
+  titleScreen = 1;
   beginSwitch = 1;
   console.log("amt salt in tank: "+poundsSaltInTank);
 }
@@ -53,13 +61,26 @@ function draw(){
 
   push();
   textSize(14);
-  text("Concentration of salt flowing in (0 - 0.5 lbs/gal)",width/2,height-20);
+  text("Concentration of salt flowing in (0 - 0.25 lbs/gal)",width/2,height-20);
+  text("Initial amount of salt in tank (0-25 lbs)",width/4-110,height-20);
   pop();
+
+  push();
+  stroke(255,0,0);
+  line(0,height-map(concentrationIn*tankVolume,0,maxSaltAmount,50,height),width,height-map(concentrationIn*tankVolume,0,maxSaltAmount,50,height));
+  pop();
+
+  push();
+  textSize(16);
+  text(round(concentrationIn*tankVolume) + " lbs",width-45, height-map(concentrationIn*tankVolume,0,maxSaltAmount,50,height)+25);
+  pop();
+
+
   if(beginSwitch==1){
     tick();
     timeCounter++;
   }
-  if(timeCounter % 60 == 0 && beginSwitch == 1){
+  if(beginSwitch == 1){
     console.log("amt salt in tank: "+poundsSaltInTank);
   }
 
@@ -83,6 +104,22 @@ function tick(){
     amount: poundsSaltInTank
    });
 }
+
+
+function concentrationChanged(){
+  concentrationIn = concentrationInSlider.value();
+  poundsSaltInTank = initialSaltSlider.value();
+  timeCounter = 1;
+  saltAmountHistory = [];
+}
+
+function saltChanged(){
+  poundsSaltInTank = initialSaltSlider.value();
+  timeCounter = 1;
+  saltAmountHistory = [];
+}
+
+
 
 function keyTyped(){
   if(keyCode == 32){
