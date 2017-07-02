30ae82f1c39afcb023f32432ba489110e3a2d8b3
brayvid <blakerayvid@gmail.com>
2017-07-02 14:47:15 -0400

    upd

--- in-dev/spring/sketch.js
+++ in-dev/spring/sketch.js
@@ -1,49 +1,68 @@
 var massSlider, frictionSlider, springSlider;
 var histories, paused;
+var spring;
+var eqPos;
+
+function preload(){
+  spring = loadImage('spring.jpg');
+}
 
 function setup(){
+  noLoop();
   createCanvas(3*windowWidth/4-25,windowHeight/2);
-  frameRate(30);
+
+  frameRate(26);
 
   massSlider = createSlider(1,10,5,0.5);
   massSlider.position(25,windowHeight/4+50);
   massSlider.style('width', (windowWidth/4-50)+'px');
   histories = [];
-  paused = false;
+  paused = true;
+  eqPos = createVector(0.045*width,0.5*height);
 }
 
 
 
 function draw(){
+  var temp;
   background(255);
+  push();
   fill(255);
   stroke(0);
-  rect(0,0,width-1,height-1);
+  rect(0,0,width-1,height-1); // border
+  pop();
+
+  push();
+  imageMode(CORNER);
   fill(100);
+  temp = displ();
+  image(spring,eqPos.x-20,61,40,eqPos.y+temp-78);
+  pop();
+
   push();
   rectMode(CENTER);
-  rect(50,65/2,25,65);
-
+  fill(100);
+  rect(eqPos.x,30,0.06*width,60);
   pop();
 
   push();
   ellipseMode(CENTER);
   fill(100,100,255);
-  ellipse(50,height/2+displ(),40);
+  ellipse(eqPos.x,eqPos.y+temp,40);
   pop();
 
   push();
   beginShape();
   noFill();
   for(var i = 0; i < histories.length; i++){
-    vertex(100 + i*((width-50)/100),height/2+histories[i]);
+    vertex(100 + (i*((width-50)/200)),height/2+histories[i]);
   }
   endShape();
   pop();
 }
 
 function displ(){
-  var pos = (height/6)*1.04*exp(-frameCount/10)*cos((frameCount/1.5));
+  var pos = 2*(height/6)*exp(-frameCount/16)*cos((frameCount/1.5));
   histories.push(pos);
   return pos;
 }
@@ -51,6 +70,7 @@ function windowResized(){
   resizeCanvas(windowWidth/2,windowHeight/2);
   massSlider.position(25,windowHeight/4+50);
   massSlider.style('width', (windowWidth/4-50)+'px');
+  eqPos.set(0.1*width,0.5*height);
 }
 
 function keyPressed(){
