324bb82714a3ba5b6fe68e5980029fd324a0d75a
brayvid <blake@Blakes-iMac.fios-router.home>
2019-11-22 16:49:09 -0500

    bug fixes

--- delivery/delivery.js
+++ delivery/delivery.js
@@ -1,9 +1,6 @@
-// Fair division of orders to drivers
-// Author: https//github.com/brayvid
-// (c) 2019 Blake Rayvid. Licensed for non-commerical use only.
-
-
-
+// Efficient division of orders to drivers
+// (c) 2019 Blake Rayvid. License is granted for non-commerical use only.
+// Author: <https//github.com/brayvid>
 
 var defaultFields = 3;
 var currentFields = 0;
@@ -12,32 +9,32 @@ var geocoder;
 
 var data;
 
+var kmeanCount = 11;
+
 var numDrivers;
 var orders;
 var coords;
+var addresses;
+var dMatrix;
+var indices;
+var groups;
 
 var storeCoords = [40.729670, -74.000450];
 var storeAddress = '116 Macdougal St';
+var cityState = "NY NY";
 
-var googleCallCounter;
+var geocodeCounter;
+var geocodingComplete = false;
+var matrixComplete = false;
+
+var filteredFinalGroups;
+
 class Order {
     constructor(name, address) {
         this.name = name;
         this.address = address;
-        // this.tip = tip;
     }
 }
-
-// class Driver {
-//     constructor(name, deliveries) {
-//         this.name = name;
-//         this.deliveries = deliveries;
-//     }
-
-//     addDelivery(delivery) {
-//         this.deliveries.push(delivery);
-//     }
-// }
 
 function addField() {
     let i = currentFields;
@@ -74,9 +71,7 @@ function removeField() {
 }
 
 function compute() {
-
-    
-    googleCallCounter = 0;
+    geocodeCounter = 0;
 
     $(window).scrollTop(0);
 
@@ -98,105 +93,247 @@ function compute() {
         return;
     }
 
-    document.getElementById("go").innerHTML = '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div></div>';
+    document.getElementById("go").innerHTML = '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div></div>';
 
-    // Get X-Y LatLng coordinates from addresses
+
+    // Get LatLng coordinates from addresses
     coords = [];
     for (let i = 0; i < orders.length; i++) {
         // console.log("Address: " + orders[i].address);
+        document.getElementById("go").innerHTML = '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="' + (i + 1) * 10 + '" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div></div>';
         geocoder.geocode({
             'address': orders[i].address
         }, function (results, status) {
             // console.log(status);
             if (status == 'OK') {
                 // console.log("Lat: " + results[0].geometry.location.lat() + ", " + "Lng: " + results[0].geometry.location.lng());
-                coords.push([Math.exp((results[0].geometry.location.lat() - storeCoords[0])), Math.exp((results[0].geometry.location.lng() - storeCoords[1]))])
+                // coords.push([Math.exp((results[0].geometry.location.lat() - storeCoords[0])), Math.exp((results[0].geometry.location.lng() - storeCoords[1]))]);
+                coords.push([(results[0].geometry.location.lat() - storeCoords[0]), (results[0].geometry.location.lng() - storeCoords[1])]);
+
             } else {
                 alert('Geocode was not successful geocoding address "' + address + '" for the following reason: ' + status);
             }
-            googleCallCounter++;
-            if(googleCallCounter == orders.length){
-                resultsTable();
+            geocodeCounter++;
+            if (geocodeCounter == orders.length) {
+                geocodingComplete = true;
+
+                // Get Distance Matrix from addresses
+                addresses = [storeAddress];
+                for (let i = 0; i < orders.length; i++) {
+                    addresses.push(orders[i].address + " " + cityState);
+                }
+                matrixService.getDistanceMatrix({
+                    origins: addresses,
+                    destinations: addresses,
+                    travelMode: 'BICYCLING',
+                    // unitSystem: google.maps.UnitSystem.IMPERIAL
+                }, function (response, status) {
+                    // console.log(status);
+                    if (status == 'OK') {
+                        // console.log("Lat: " + results[0].geometry.location.lat() + ", " + "Lng: " + results[0].geometry.location.lng());
+                        dMatrix = response.rows;
+                        matrixComplete = true;
+                        analyzeResults();
+                    } else {
+                        alert('Distance Matrix call was not successful for the following reason: ' + status);
+                    }
+                });
                 // console.log(coords);
             }
         });
     }
-
-    // console.log(coords);
-
-
-    // Make data out of coordinates
-
-
-
-
-
-
-    // data = []; // fill in proper syntax for k-means clustering
-
-    // for (let i = 0; i < orders.length; i++) {
-    //     data.push([parseFloat(orders[i].address), parseFloat(orders[i].address)]); // data.push([x, x])
-    // }
-
-    // console.log(data);
-
-
 }
 
-function resultsTable() {
-    kmeans = new KMeans({
-        // canvas: document.getElementById('canvas'),
-        data: coords,
-        k: numDrivers
-    });
+function analyzeResults() {
 
-    // console.log(kmeans.assignments);
-    console.log(kmeans.assignments);
-    let indices = [];
+    document.getElementById("go").innerHTML = '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div></div>';
+
+    allKMeans = new Array(kmeanCount);
+    for (let i = 0; i < kmeanCount; i++) {
+        allKMeans[i] = new KMeans({
+            data: coords,
+            k: numDrivers
+        });
+    }
+
+    // console.log(coords);
+    // console.log("allKMeans.assignments:\n");
+    // for (let i = 0; i < kmeanCount; i++) {
+    //     console.log(allKMeans[i].assignments);
+
+    // }
+
+    let uniques = [];
+    let freqs = [];
+    for (let i = 0; i < kmeanCount; i++) {
+        let activeArray = allKMeans[i].assignments;
+        let pos = uniques.indexOf(activeArray);
+        if (pos != -1) {
+            uniques[pos] = activeArray;
+            freqs[pos]++;
+        } else {
+            uniques.push(activeArray);
+            freqs.push(1);
+        }
+    }
+
+    let mostFrequentKmean;
+    let max = 0;
+    for (let i = 0; i < uniques.length; i++) {
+        if (freqs[i] >= max) {
+            max = freqs[i];
+            mostFrequentKmean = uniques[i];
+        }
+    }
+
+    console.log("mostFrequentKmean:\n");
+    console.log(mostFrequentKmean);
+
+
+    indices = [];
     for (let i = 0; i < numDrivers; i++) {
         indices[i] = [];
         for (let j = 0; j < orders.length; j++) {
-            if (kmeans.assignments[j] == i) {
+            if (allKMeans[j].assignments[j] == i) {
                 indices[i].push(j);
             }
         }
     }
-    // console.log("Indices:\n");
+
+    // console.log("indices:\n");
     // console.log(indices);
 
-
-    // Generate table headers
-    document.getElementById("go").innerHTML = '<table class="table table-condensed"><tbody id="tablebody"></tbody></table>';
-    for (let i = 0; i < numDrivers; i++) {
-        let headNode = document.createElement("TH");
-        let headText = document.createTextNode("Driver " + (i + 1));
-        headNode.appendChild(headText);
-        document.getElementById("tablebody").appendChild(headNode);
-    }
-
-    // distMatrix(orders);
-
-    // drivers = [];
-
-    // for (let i = 0; i < numDrivers; i++) {
-    //     drivers.push(new Driver("Driver " + (i + 1), []));
-    // }
-
-
-    // Display the computed partition
-
-    // Find max orders for one driver
-    let maxToOneDriver = 0;
-    for (let i = 0; i < indices.length; i++) {
-        let thisLength = indices[i].length;
-        if (thisLength > maxToOneDriver) {
-            maxToOneDriver = thisLength;
+    let matrixTimes = [],
+        averageTimes = [];
+    for (let i = 0; i < orders.length + 1; i++) {
+        matrixTimes.push([]);
+        averageTimes.push([]);
+        for (let j = 0; j < orders.length + 1; j++) {
+            matrixTimes[i].push(dMatrix[i].elements[j].duration.value);
+            averageTimes[i].push(0);
         }
     }
 
-    let ordersPerDriver = [];
+    // console.log("matrixTimes:\n");
+    // console.log(matrixTimes);
+
+    // let averageTimes = matrixTimes;
+
+    for (let i = 0; i < matrixTimes.length; i++) {
+        for (let j = 0; j < matrixTimes.length; j++) {
+            if (j < i) {
+                averageTimes[j][i] = 0;
+            } else {
+                averageTimes[j][i] = (matrixTimes[i][j] + matrixTimes[j][i]) / 2;
+            }
+        }
+    }
+
+    console.log("averageTimes:\n");
+    console.log(averageTimes);
+
+
+    // Determine global min and max
+    let avgMax = 0;
+    let bigNum = 999999999;
+    let avgMin = bigNum;
+    let maxIndex;
+    let minIndex;
+
+    for (let i = 1; i < averageTimes.length; i++) {
+        for (let j = 1; j < i; j++) {
+            if (averageTimes[i][j] >= avgMax) {
+                avgMax = averageTimes[i][j];
+                maxIndex = [i, j];
+            }
+            if (averageTimes[i][j] <= avgMin) {
+                avgMin = averageTimes[i][j];
+                minIndex = [i, j];
+            }
+        }
+    }
+
+    // console.log("avgMax: " + avgMax);
+    // console.log("maxIndex: " + maxIndex);
+    // console.log("avgMin: " + avgMin);
+    // console.log("minIndex: " + minIndex);
+
+
+    // Origin and destination are same group for element with minimum nonzero average time.
+
+    let matrixGroupIndices = new Array(numDrivers);
     for (let i = 0; i < numDrivers; i++) {
-        ordersPerDriver[i] = indices[i].length;
+        matrixGroupIndices[i] = -1;
+    }
+
+    let matrixMidrange = (avgMax + avgMin) / 2;
+
+    if (orders.length == 1) {
+        matrixGroupIndices[getRandomChoice(numDrivers)] = 0;
+    } else {
+        groups = [];
+        for (let i = 1; i < averageTimes.length; i++) {
+            for (let j = 1; j < i; j++) {
+                if (averageTimes[i][j] < matrixMidrange) {
+                    // Small time -> same group
+                    if (groups.length != 0) {
+                        for (let k = 0; k < groups.length; k++) {
+                            if (groups[k].includes(i) && groups[k].includes(j)) {
+
+                            } else if (groups[k].includes(i)) {
+                                groups[k].push(j);
+                            } else if (groups[k].includes(j)) {
+                                groups[k].push(i);
+                            } else {
+                                groups.push([i, j]);
+                            }
+                        }
+                    } else {
+                        groups.push([i, j]);
+                    }
+                }
+            }
+            // console.log("groups " + i + "\n");
+            // console.log(groups);
+        }
+        // 
+        for (let i = 0; i < orders.length; i++) {
+            let contained = false;
+            for (let j = 0; j < groups.length; j++) {
+                if (groups[j].includes(i + 1)) {
+                    contained = true;
+                }
+            }
+            if (!contained) {
+                groups.push([i + 1]);
+            }
+        }
+
+        for(let i = 0; i < groups.length; i++){
+            groups[i].sort();
+        }
+
+        // console.log("\n");
+        // console.log("groups:\n");
+        // console.log(groups);
+    }
+
+    drawTable();
+}
+
+function drawTable() {
+
+    // Find max orders one driver is taking
+    let maxToOneDriver = 0;
+    let ordersPerDriver = [];
+    for (let i = 0; i < groups.length; i++) {
+        ordersPerDriver.push(groups[i].length);
+        if (groups[i].length > maxToOneDriver) {
+            maxToOneDriver = groups[i].length;
+        }
+    }
+    for (let i = 0; i < numDrivers - ordersPerDriver.length; i++) {
+        ordersPerDriver.push(0);
     }
 
     let assignMap = [];
@@ -216,13 +353,22 @@ function resultsTable() {
         counter.push(0);
     }
 
+    // Table header
+    document.getElementById("go").innerHTML = '<table class="table table-condensed"><tbody id="tablebody"></tbody></table>';
+    for (let i = 0; i < numDrivers; i++) {
+        let headNode = document.createElement("TH");
+        let headText = document.createTextNode("Driver " + (i + 1));
+        headNode.appendChild(headText);
+        document.getElementById("tablebody").appendChild(headNode);
+    }
 
+    // Table body
     for (let i = 0; i < maxToOneDriver; i++) { //  each row
         let bodyNode = document.createElement("TR");
         for (let j = 0; j < numDrivers; j++) {
             let bodyData = document.createElement("TD");
             if (assignMap[i][j] == 1) {
-                let bodyText = document.createTextNode(orders[indices[j][counter[j]]].name);
+                let bodyText = document.createTextNode(orders[groups[j][i] - 1].name);
                 counter[j]++;
                 bodyData.appendChild(bodyText);
             }
@@ -230,66 +376,29 @@ function resultsTable() {
         }
         document.getElementById("tablebody").appendChild(bodyNode);
     }
-
-
-
-
-}
-
-function bikeDist(a, b) {
-    // Google Maps API call here
-    return Math.random();
-}
-
-function distMatrix(orders) {
-    let originsArr = [storeAddress + " NY NY"];
-    let destsArr = originsArr;
-    for (let i = 0; i < orders.length; i++) {
-        originsArr.push(orders[i].address + " NY NY");
-        destsArr[i + 1] = originsArr[i + 1];
-    }
-    service.getDistanceMatrix({
-        origins: originsArr,
-        destinations: destsArr,
-        travelMode: 'BICYCLING',
-    }, callback);
-}
-//
-
-function callback(response, status) {
-    if (status == 'OK') {
-        var origins = response.originAddresses;
-        var destinations = response.destinationAddresses;
-
-        for (var i = 0; i < origins.length; i++) {
-            var results = response.rows[i].elements;
-            for (var j = 0; j < results.length; j++) {
-                var element = results[j];
-                var distance = element.distance.text;
-                var duration = element.duration.text;
-                var from = origins[i];
-                var to = destinations[j];
-            }
-        }
-    }
 }
 
 function googleReady() {
     geocoder = new google.maps.Geocoder();
+    matrixService = new google.maps.DistanceMatrixService();
     console.log("Google Maps API ready");
 }
 
-// // rows = origins, columns = destinations
-// let n = orders.length;
-// let mat = new Array(n);
-// for (let i = 0; i < n; i++) {
-//     mat[i] = new Array(n);
-//     for (let j = 0; j < n; j++) {
-//         if (i == j) {
-//             mat[i][j] = 0;
-//         } else {
-//             mat[i][j] = bikeDist(orders[i].address, orders[j].address);
-//         }
-//     }
-// }
-// return mat;
+function arraysMatch(arr1, arr2) {
+
+    // Check if the arrays are the same length
+    if (arr1.length !== arr2.length) return false;
+
+    // Check if all items exist and are in the same order
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+
+    // Otherwise, return true
+    return true;
+
+}
+
+function getRandomChoice(a) {
+    return Math.floor(Math.random() * (a + 1));
+}
