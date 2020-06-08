e503f64a7f9d8edc941c4219c841e0ef2478cc4d
brayvid <brayvid@users.noreply.github.com>
2020-06-08 19:18:45 -0400

    take log for stability

--- threebody/sketch.js
+++ threebody/sketch.js
@@ -2,11 +2,11 @@ function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
     frameRate(60);
     background(0);
-    mass = [100, 100, 100];
-    cylinderRadius = 0.5;
-    coordLen = 20;
-    size = 10;
-    cols = [color(255, 0, 0), color(255), color(0, 0, 255)]
+    bodyMass = [1, 1, 1];
+    axisRadius = 0.5;
+    axisLength = 20;
+    bodySize = 5;
+    cols = [color(255, 0, 0), color(0, 0, 255), color(255)]
     posVec = [
         createVector(0, 0, 0),
         createVector(0, 0, 0),
@@ -42,18 +42,20 @@ function draw() {
     // orbitControl();
 
     // posVec = posBuffer;
+    posVec = posBuffer;
 
     if (entered[0] == true && entered[1] == true && entered[2] == true) {
         orbitControl();
-        updatePosition(0, accel(posVec[0], posVec[1], posVec[2], mass[1], mass[2]));
-        updatePosition(1, accel(posVec[1], posVec[2], posVec[0], mass[2], mass[0]));
-        updatePosition(2, accel(posVec[2], posVec[0], posVec[1], mass[0], mass[1]));
+        updatePosition(0, accel(posVec[0], posVec[1], posVec[2], bodyMass[1], bodyMass[2]));
+        updatePosition(1, accel(posVec[1], posVec[2], posVec[0], bodyMass[2], bodyMass[0]));
+        updatePosition(2, accel(posVec[2], posVec[0], posVec[1], bodyMass[0], bodyMass[1]));
+        translate(getCentroid());
         drawTriangle(posVec);
     }
 
 
     velVec = velBuffer;
-    posVec = posBuffer;
+
 
     for (let i = 0; i < posVec.length; i++) {
         if (entered[i]) {
@@ -64,12 +66,17 @@ function draw() {
 
     // console.log(posVec[0].x);
 
-    drawAxes();
+    // drawAxes();
+}
+
+function getCentroid() {
+    let centroid = p5.Vector.add(p5.Vector.add(posVec[0].mult(bodyMass[0]), posVec[1].mult(bodyMass[0])), posVec[2].mult(bodyMass[0])).mult(-1 / (bodyMass[0] + bodyMass[1] + bodyMass[2]));
+    return centroid;
 }
 
 function accel(posA, posB, posC, mB, mC) {
-    const c1 = -mB / p5.Vector.sub(posA, posB).magSq();
-    const c2 = -mC / p5.Vector.sub(posA, posC).magSq();
+    const c1 = -mB / Math.log(p5.Vector.sub(posA, posB).magSq());
+    const c2 = -mC / Math.log(p5.Vector.sub(posA, posC).magSq());
     return p5.Vector.add(p5.Vector.sub(posA, posB).normalize().mult(c1), p5.Vector.sub(posA, posC).normalize().mult(c2));
 }
 
@@ -91,17 +98,25 @@ function drawVector(v, c) {
     noStroke();
     translate(v.x, v.y, v.z);
     fill(c);
-    sphere(size);
+    sphere(bodySize);
     pop();
 }
 
 function drawTriangle(vvv) {
+
+    const surfaceNormal = p5.Vector.cross(p5.Vector.sub(vvv[1], vvv[0]), p5.Vector.sub(vvv[2], vvv[0])).normalize().mult(0.01);
     push();
     noStroke();
-    fill(100);
+    fill(50);
     beginShape();
     for (let i = 0; i < 3; i++) {
-        vertex(vvv[i].x, vvv[i].y, vvv[i].z);
+        vertex(vvv[i].x - surfaceNormal.x, vvv[i].y - surfaceNormal.y, vvv[i].z - surfaceNormal.z);
+    }
+    endShape();
+    fill(150);
+    beginShape();
+    for (let i = 0; i < 3; i++) {
+        vertex(vvv[i].x + surfaceNormal.x, vvv[i].y + surfaceNormal.y, vvv[i].z + surfaceNormal.z);
     }
     endShape();
     pop();
@@ -113,15 +128,15 @@ function drawTriangle(vvv) {
 //     console.log();
 // }
 
-function mouseClicked() {
+function touchEnded() {
     if (!entered[0]) {
-        posVec[0] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 4, windowWidth / 4));
+        posVec[0] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 6, windowWidth / 6));
         entered[0] = true;
     } else if (!entered[1]) {
-        posVec[1] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 4, windowWidth / 4));
+        posVec[1] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 6, windowWidth / 6));
         entered[1] = true;
     } else if (!entered[2]) {
-        posVec[2] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 4, windowWidth / 4));
+        posVec[2] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 6, windowWidth / 6));
         entered[2] = true;
         // orbitControl();
     }
@@ -140,32 +155,32 @@ function drawAxes() {
     push();
     rotateZ(HALF_PI);
     ambientMaterial(220, 220, 220);
-    cylinder(cylinderRadius, coordLen * 24);
-    translate(0, -coordLen * 12, 0);
-    cone(cylinderRadius * 3, -cylinderRadius * 9);
-    translate(0, coordLen * 24, 0);
-    cone(cylinderRadius * 3, cylinderRadius * 9);
+    cylinder(axisRadius, axisLength * 24);
+    translate(0, -axisLength * 12, 0);
+    cone(axisRadius * 3, -axisRadius * 9);
+    translate(0, axisLength * 24, 0);
+    cone(axisRadius * 3, axisRadius * 9);
     pop();
 
     // Y-axis (green)
     push();
     ambientMaterial(220, 220, 220);
-    cylinder(cylinderRadius, coordLen * 24);
-    translate(0, -coordLen * 12, 0);
-    cone(cylinderRadius * 3, -cylinderRadius * 9);
-    translate(0, coordLen * 24, 0);
-    cone(cylinderRadius * 3, cylinderRadius * 9);
+    cylinder(axisRadius, axisLength * 24);
+    translate(0, -axisLength * 12, 0);
+    cone(axisRadius * 3, -axisRadius * 9);
+    translate(0, axisLength * 24, 0);
+    cone(axisRadius * 3, axisRadius * 9);
     pop();
 
     // Z-axis (blue)
     push();
     rotateX(HALF_PI);
     ambientMaterial(220, 220, 220);
-    cylinder(cylinderRadius, coordLen * 24);
-    translate(0, -coordLen * 12, 0);
-    cone(cylinderRadius * 3, -cylinderRadius * 9);
-    translate(0, coordLen * 24, 0);
-    cone(cylinderRadius * 3, cylinderRadius * 9);
+    cylinder(axisRadius, axisLength * 24);
+    translate(0, -axisLength * 12, 0);
+    cone(axisRadius * 3, -axisRadius * 9);
+    translate(0, axisLength * 24, 0);
+    cone(axisRadius * 3, axisRadius * 9);
     pop();
 
     pop();
