8d9efed5ce6c918a47eed15f262eba4cf3875d62
brayvid <brayvid@users.noreply.github.com>
2020-06-12 16:10:17 -0400

    v0.3

--- threebody/sketch.js
+++ threebody/sketch.js
@@ -2,19 +2,16 @@ let introFont;
 
 function preload() {
     introFont = loadFont('assets/cmunss.otf');
-    img1 = loadImage('assets/img1.jpg');
-    img2 = loadImage('assets/img2.jpg');
+    // img1 = loadImage('assets/img1.jpg');
+    // img2 = loadImage('assets/img2.jpg');
 
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
     frameRate(60);
-    background(0);
     bodyMass = [1, 1, 1];
-    bodySize = Math.round(Math.log(windowWidth + windowHeight + 100));
-    // axisRadius = 0.5;
-    // axisLength = 20;
+    bodySize = Math.round(Math.log(windowWidth + windowHeight + 1000));
     cols = [color(81, 119, 166), color(70, 133, 62), color(220, 110, 121)]
     posVec = [
         createVector(0, 0, 0),
@@ -32,6 +29,7 @@ function setup() {
         createVector(0, 0, 0),
         createVector(0, 0, 0)
     ];
+
     velBuffer = [
         createVector(0, 0, 0),
         createVector(0, 0, 0),
@@ -39,25 +37,21 @@ function setup() {
     ];
 
     entered = [false, false, false];
-    beginTouch = [0, 0];
     touchAllowed = true;
     textureMode(IMAGE);
-    // textureSize()
     textureWrap(REPEAT);
+    background(0);
 }
 
 function draw() {
     background(0);
-    // Lighting
-
     if (entered[0] == false) {
         push();
         fill(255);
-        // translate(-windowWidth / 2, -windowHeight / 2, 0);
         textAlign(CENTER, CENTER);
         textFont(introFont, 24);
-        text("3-body system under gravity:\nTap on three regions.", 0, -40);
-        text("v0.2", 0, windowHeight / 2 - 80);
+        text("3-body system under gravity:\nTap on three places to choose\nan initial position for each body.\nAll start at rest.", 0, -40);
+        text("v0.3", 0, windowHeight / 2 - 80);
         pop();
     }
 
@@ -72,7 +66,7 @@ function draw() {
         updatePosition(1, accel(posVec[1], posVec[2], posVec[0], bodyMass[2], bodyMass[0]));
         updatePosition(2, accel(posVec[2], posVec[0], posVec[1], bodyMass[0], bodyMass[1]));
         translate(getCentroid());
-        drawTriangle(posVec);
+        // drawTriangle(posVec);
     }
 
     velVec = velBuffer;
@@ -82,7 +76,6 @@ function draw() {
             drawBody(posVec[i], cols[i]);
         }
     }
-    // drawAxes();
 }
 
 function getCentroid() {
@@ -110,30 +103,22 @@ function drawBody(v, c) {
     pop();
 }
 
-function drawTriangle(vvv) {
-
-    const surfaceNormal = p5.Vector.cross(p5.Vector.sub(vvv[1], vvv[0]), p5.Vector.sub(vvv[2], vvv[0])).normalize().mult(0.05);
-    push();
-    texture(img1);
-    // noStroke();
-    // fill(50);
-    beginShape();
-    for (let i = 0; i < 3; i++) {
-        vertex(vvv[i].x - surfaceNormal.x, vvv[i].y - surfaceNormal.y, vvv[i].z - surfaceNormal.z);
-    }
-    endShape();
-    texture(img2);
-    beginShape();
-    for (let i = 0; i < 3; i++) {
-        vertex(vvv[i].x + surfaceNormal.x, vvv[i].y + surfaceNormal.y, vvv[i].z + surfaceNormal.z);
-    }
-    endShape();
-    pop();
-}
-
-// function touchStarted() {
-//     // verifiedTouch = true;
-//     // justTouched = false;
+// function drawTriangle(vvv) {
+//     const surfaceNormal = p5.Vector.cross(p5.Vector.sub(vvv[1], vvv[0]), p5.Vector.sub(vvv[2], vvv[0])).normalize().mult(0.05);
+//     push();
+//     texture(img1);
+//     beginShape();
+//     for (let i = 0; i < 3; i++) {
+//         vertex(vvv[i].x - surfaceNormal.x, vvv[i].y - surfaceNormal.y, vvv[i].z - surfaceNormal.z);
+//     }
+//     endShape();
+//     texture(img2);
+//     beginShape();
+//     for (let i = 0; i < 3; i++) {
+//         vertex(vvv[i].x + surfaceNormal.x, vvv[i].y + surfaceNormal.y, vvv[i].z + surfaceNormal.z);
+//     }
+//     endShape();
+//     pop();
 // }
 
 function touchEnded() {
@@ -156,43 +141,3 @@ function touchEnded() {
 function resetTouchState() {
     touchAllowed = true;
 }
-
-
-// function drawAxes() {
-//     push();
-//     noStroke();
-
-//     // X-axis
-//     push();
-//     rotateZ(HALF_PI);
-//     ambientMaterial(220, 220, 220);
-//     cylinder(axisRadius, axisLength * 24);
-//     translate(0, -axisLength * 12, 0);
-//     cone(axisRadius * 3, -axisRadius * 9);
-//     translate(0, axisLength * 24, 0);
-//     cone(axisRadius * 3, axisRadius * 9);
-//     pop();
-
-//     // Y-axis (green)
-//     push();
-//     ambientMaterial(220, 220, 220);
-//     cylinder(axisRadius, axisLength * 24);
-//     translate(0, -axisLength * 12, 0);
-//     cone(axisRadius * 3, -axisRadius * 9);
-//     translate(0, axisLength * 24, 0);
-//     cone(axisRadius * 3, axisRadius * 9);
-//     pop();
-
-//     // Z-axis (blue)
-//     push();
-//     rotateX(HALF_PI);
-//     ambientMaterial(220, 220, 220);
-//     cylinder(axisRadius, axisLength * 24);
-//     translate(0, -axisLength * 12, 0);
-//     cone(axisRadius * 3, -axisRadius * 9);
-//     translate(0, axisLength * 24, 0);
-//     cone(axisRadius * 3, axisRadius * 9);
-//     pop();
-
-//     pop();
-// }
