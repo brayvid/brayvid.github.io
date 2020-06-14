87f97bd71f67692064b61e44a389626972b87f12
brayvid <brayvid@users.noreply.github.com>
2020-06-14 11:01:53 -0400

    trails

--- misc/threebody/sketch.js
+++ misc/threebody/sketch.js
@@ -12,7 +12,12 @@ function setup() {
     frameRate(60);
     bodyMass = [1, 1, 1];
     bodySize = Math.round(Math.log(windowWidth + windowHeight + 1000));
-    cols = [color(81, 119, 166), color(70, 133, 62), color(220, 110, 121)]
+    cols = [color(81, 119, 166), color(70, 133, 62), color(220, 110, 121)];
+    trailCols = [
+        [81, 119, 166, 0],
+        [70, 133, 62, 0],
+        [220, 110, 121, 0]
+    ]
     posVec = [
         createVector(0, 0, 0),
         createVector(0, 0, 0),
@@ -36,10 +41,14 @@ function setup() {
         createVector(0, 0, 0)
     ];
 
+    posHistory = [];
+    centroidHistory = [];
+
     entered = [false, false, false];
     touchAllowed = true;
-    textureMode(IMAGE);
-    textureWrap(REPEAT);
+    maxTrailLength = 32;
+    // textureMode(IMAGE);
+    // textureWrap(REPEAT);
     background(0);
 }
 
@@ -51,7 +60,7 @@ function draw() {
         textAlign(CENTER, CENTER);
         textFont(introFont, 24);
         text("3-body system under gravity:\nTap on three places to choose\nan initial position for each body.\nAll start at rest.", 0, -40);
-        text("v0.3", 0, windowHeight / 2 - 80);
+        text("v0.4", 0, windowHeight / 2 - 80);
         pop();
     }
 
@@ -61,12 +70,30 @@ function draw() {
     posVec = posBuffer;
 
     if (entered[0] == true && entered[1] == true && entered[2] == true) {
-        orbitControl();
+        // orbitControl();
         updatePosition(0, accel(posVec[0], posVec[1], posVec[2], bodyMass[1], bodyMass[2]));
         updatePosition(1, accel(posVec[1], posVec[2], posVec[0], bodyMass[2], bodyMass[0]));
         updatePosition(2, accel(posVec[2], posVec[0], posVec[1], bodyMass[0], bodyMass[1]));
-        translate(getCentroid());
-        // drawTriangle(posVec);
+        let centr = getCentroid();
+        let pos = [];
+        for (let i = 0; i < posVec.length; i++) {
+            pos.push(p5.Vector.add(posVec[i], centr));
+        }
+
+        centroidHistory.push(centr);
+        posHistory.push(pos);
+
+        if (posHistory.length >= maxTrailLength) {
+            posHistory.splice(0, 1);
+        }
+
+        if (posHistory.length > 2) {
+            // drawTriangle(posVec);
+            for (let i = 0; i < posVec.length; i++) {
+                drawTrail(i);
+            }
+        }
+        translate(centr);
     }
 
     velVec = velBuffer;
@@ -74,8 +101,29 @@ function draw() {
     for (let i = 0; i < posVec.length; i++) {
         if (entered[i]) {
             drawBody(posVec[i], cols[i]);
+
         }
     }
+}
+
+function drawTrail(bod) {
+    push();
+    noStroke();
+    let current = posHistory[posHistory.length - 1][bod];
+    translate(current.x, current.y, current.z);
+    let stepsBack = 0;
+    let diff;
+    let alph;
+    for (let j = posHistory.length - 2; j >= 0; j--) {
+        diff = p5.Vector.sub(posHistory[j][bod], posHistory[j + 1][bod]);
+        alph = round(map(stepsBack, 0, maxTrailLength, 200, 0, true));
+        translate(diff.x, diff.y, -diff.z);
+        trailCols[bod][3] = alph;
+        fill(trailCols[bod][0], trailCols[bod][1], trailCols[bod][2], trailCols[bod][3]);
+        sphere(1);
+        stepsBack++;
+    }
+    pop();
 }
 
 function getCentroid() {
@@ -124,13 +172,16 @@ function drawBody(v, c) {
 function touchEnded() {
     if (touchAllowed) {
         if (!entered[0]) {
-            posVec[0] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 6, windowWidth / 6));
+            posVec[0] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowHeight / 4, windowHeight / 4));
+            console.log("Body 1: [" + posVec[0].x + ", " + posVec[0].y + ", " + posVec[0].z + "]");
             entered[0] = true;
         } else if (!entered[1]) {
-            posVec[1] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 6, windowWidth / 6));
+            posVec[1] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowHeight / 4, windowHeight / 4));
+            console.log("Body 2: [" + posVec[1].x + ", " + posVec[1].y + ", " + posVec[1].z + "]");
             entered[1] = true;
         } else if (!entered[2]) {
-            posVec[2] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowWidth / 6, windowWidth / 6));
+            posVec[2] = createVector(mouseX - windowWidth / 2, mouseY - windowHeight / 2, random(-windowHeight / 4, windowHeight / 4));
+            console.log("Body 3: [" + posVec[2].x + ", " + posVec[2].y + ", " + posVec[2].z + "]");
             entered[2] = true;
         }
         touchAllowed = false;
