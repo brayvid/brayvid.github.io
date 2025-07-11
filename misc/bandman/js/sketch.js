2c2743b4719aa1113dc18e502274343a3f18e564
Blake Rayvid <brayvid@users.noreply.github.com>
2025-07-11 11:16:58 -0400

    Fix bandman

--- misc/bandman/js/sketch.js
+++ misc/bandman/js/sketch.js
@@ -1,3 +1,5 @@
+// --- UNCHANGED CODE AT THE TOP ---
+
 var loc = "./data/ibm.json";
 var data;
 var numFrames = 20;
@@ -25,6 +27,11 @@ var playerSize = 15;
 var dateNow;
 
 function preload(){
+    // NOTE: This assumes 'ibm_data' is a global variable
+    // from a script included in your HTML, as the original code implies.
+    // If you are loading from a file, it should be:
+    // data = loadJSON(loc); 
+    // And you'd access it differently in setup(). For now, keeping original logic.
     data = ibm_data["Technical Analysis: BBANDS"];
 }
 
@@ -43,22 +50,13 @@ function setup(){
 
     for (var i = 0; i <= numFrames + 1; i++){
         xvals.push(map(i, 0, numFrames, 0, windowWidth));
-        // console.log(formatDate(dateNow));
         currentData = data[formatDate(dateNow)]
         shownValues["upper"].push(parseFloat(currentData["Real Upper Band"]));
         shownValues["middle"].push(parseFloat(currentData["Real Middle Band"]));
         shownValues["lower"].push(parseFloat(currentData["Real Lower Band"]));
         dateNow = addDays(dateNow, 7);
     }
-
-    // console.log(shownValues);
-
-    // bounds[0] = min(shownValues["lower"]);
-    // bounds[1] = max(shownValues["upper"]);
-    // oldBounds = bounds;
     
-    // centerY = (bounds[0] + bounds[1]) / 2;
-    // console.log(bounds);
     jx = map(jxFrame - 1, 0, numFrames, 0, windowWidth);
     displaced = 0;
     fill(255,0,0);
@@ -69,25 +67,25 @@ function setup(){
     initScale = 2 * (shownValues["upper"][jxFrame - 1] - shownValues["middle"][jxFrame - 1]);
 }
 
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+// +++ NEW HELPER FUNCTION TO CHECK ALL INPUTS +++
+// ++++++++++++++++++++++++++++++++++++++++++++++
+function isGoingUp() {
+  // Check for equals key (keyCode 187)
+  // Check for space bar (keyCode 32)
+  // Check for mouse press OR screen touch
+  return keyIsDown(187) || keyIsDown(32) || mouseIsPressed;
+}
+
+
 function draw(){
     translate(0, windowHeight / 2);
     background(0);
     var yvals = [];
     for (var i = 0; i <= numFrames + 1; i++){
-
-        var thisH = 2 * (shownValues["upper"][i] - shownValues["middle"][i]);
-        
-        // centerY0 = map(i, 0 , numFrames - 2, (oldBounds[0] + oldBounds[1]) / 2 , (bounds[0] + bounds[1]) / 2);
-        // centerY1 = map(i + 1, 0 , numFrames - 2, (oldBounds[0] + oldBounds[1]) / 2 , (bounds[0] + bounds[1]) / 2);
-
-        // u0.push((windowHeight / 2) - map(shownValues["upper"][i] - centerY0 , 0 , bounds[1] - centerY , 0 , windowHeight / 2)  - displaced);
-        // u1.push((windowHeight / 2) - map(shownValues["upper"][i+1] - centerY1 , 0 , bounds[1] - centerY , 0 , windowHeight / 2)  - displaced);        
-
-        // l0.push((windowHeight / 2) + map(centerY - shownValues["lower"][i], 0 , centerY0 - bounds[0] , 0 , windowHeight / 2)  - displaced);
-        // l1.push((windowHeight / 2) + map(centerY - shownValues["lower"][i+1], 0 , centerY1 - bounds[0] , 0 , windowHeight / 2)  - displaced)
         var thisTop = displaced - (exp(map(shownValues["upper"][i], 0, 85, 0, 4)) + 15);
         var thisBottom = displaced + (exp(map(shownValues["lower"][i], 0, 85, 0, 4)) + 15);
-
         yvals.push([thisTop, thisBottom]);
     }
     
@@ -99,36 +97,33 @@ function draw(){
     }
     pop();
 
-// (intraFrameCt + 1) * ((windowWidth / numFrames) / (inverseFR - 1))
     var outBounds = map(intraFrameCt , 0, inverseFR - 1, yvals[jxFrame - 1][0], yvals[jxFrame][0]) >= -(playerSize / 2) || map(intraFrameCt , 0, inverseFR - 1, yvals[jxFrame - 1][1], yvals[jxFrame][1]) <= playerSize / 2;
-     // var outBounds = false;
     
     if (!outBounds){
-        // background(0);
-
         push();
         fill(255);
         text((floor((dateNow.getTime() - startDate.getTime()) / (1000*60*60*24*7))-22).toString(), windowWidth / 2, -(windowHeight / 2) + 30);
         pop();
-
-        if (keyIsDown(187)) {
+        
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // +++ MODIFIED THIS 'IF' BLOCK TO USE THE NEW FUNCTION +++
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        if (isGoingUp()) { // This now checks for '=', space, or touch
             displaced += 1.5 * log(upCount);
             upCount += 1;
             fallCount = 0;
-            // console.log("UP")
         } else{
             upCount = 1;
             fallCount += 1;
         }
     
+        // This 'down' control was not requested to change, so it's left as is.
         if (keyIsDown(189)) {
             displaced -= log(downCount);
             downCount += 1;
-            // fallCount = 0;
-            // console.log("DOWN")
+            fallCount += 0.5; // Adjusted to be consistent with original logic
         }else{
             downCount = 1;
-            fallCount += 0.5;
         }
     
         displaced -= 0.075 * fallCount;
@@ -139,14 +134,6 @@ function draw(){
     }else{
         gameOver(yvals);
     }
-    // oldBounds = bounds;
-    // bounds[0] = min(shownValues["lower"]);
-    // bounds[1] = max(shownValues["upper"]);
-    // centerY = (bounds[0] + bounds[1]) / 2;
-
-    // centerY = (bounds[0] + bounds[1]) / 2;
-
-
 
     intraFrameCt ++;
 
@@ -174,6 +161,8 @@ function draw(){
         frameCount ++;   
     }  
 }
+
+// --- UNCHANGED CODE AT THE BOTTOM ---
 
 function addDays(date, days) {
     var result = new Date(date);
